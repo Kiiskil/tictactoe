d6d4fa6234102a277f44cfb31a86c13c984f71ec
L8317 <l8317@student.jamk.fi>
2019-02-16 18:50:00 +0200

    bug

--- Tile.js
+++ Tile.js
@@ -35,10 +35,10 @@ Tile.prototype.press = function(turn) {
 Tile.prototype.win = function(turn){
     let k = 0;
     for(let i=0; i < winline; i++){
-        if(locx+i < grid_size && grid[this.locx+i][this.locy].class == turn){
+        if(this.locx+i < grid_size && grid[this.locx+i][this.locy].class == turn){
             k++;
         }
-        if(locx-i > 0 && grid[this.locx-i][this.locy].class == turn){
+        if(this.locx-i > 0 && grid[this.locx-i][this.locy].class == turn){
             k++;
         }
         if(k == winline){
