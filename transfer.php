000759140cb36f49f927ef255765cd0f07eb5f26
L8317 <l8317@student.jamk.fi>
2019-02-16 18:11:50 +0200

    h

--- transfer.php
+++ transfer.php
@@ -5,8 +5,8 @@
 Winning line of symbols: <?php echo $_POST["winline"]; ?>
 
 <script type="text/javascript">
-let grid_size= "<? $_POST['grid_size']?>";
-let winline = "<?php $_POST['winline']?>";
+let grid_size= "<?php $_POST["grid_size"]?>";
+let winline = "<?php $_POST["winline"]?>";
 </script>
 <script src='https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.3/p5.js'></script>
 <script src='sketch.js'></script>
