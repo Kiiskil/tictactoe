ed57fc664cbe469920c4d37593468c920290d3cb
L8317 <l8317@student.jamk.fi>
2019-02-16 20:55:43 +0200

    first test on NN

--- sketch.js
+++ sketch.js
@@ -1,9 +1,16 @@
 //TIC-TAC-TOE
+//Basic Tic-Tac-Toe
+//Made with PHP, JS (P5 -library) and Toy-Neural-Network by Coding Train 
+//PHP for getting grid size and amount required for the win from the user.
+//Toy-Neural-Network by Coding Train https://github.com/CodingTrain/Toy-Neural-Network-JS
 let grid;
 let cols;
 let rows;
 let w = 30;
 let turn;
+let brain;
+
+let grid_size = 15;
 //let grid_size= "<?php echo $_POST['grid_size']?>";
 //let winline = "<?php echo $_POST['winline']?>";
 
@@ -18,6 +25,10 @@ function setup(){
             grid[i][j] = new Tile(i * w, j * w, w);
         }
     }
+
+    //NeuralNetwork(inputs,hidden layers, outputs)
+    //NeuralNetwork(array of numbers, preferable normalized between zero and one, 3 hidden layers(?) and x,y value as output)
+    brain = new NeuralNetwork(grid_size*grid_size,3,2);
 }
 
 function make2DArray(cols, rows){
@@ -28,22 +39,75 @@ function make2DArray(cols, rows){
     return arr;
 }
 
-function mousePressed() {
+function gridizise(arr){
+    let arr_new = new Array(grid_size*grid_size);
+    let ind_new = 0;
     for(let i=0; i < cols; i++){
         for(let j=0; j < rows; j++){
-            if(grid[i][j].locx==floor(mouseX/w) && grid[i][j].locy == floor(mouseY/w)){
-                grid[i][j].press(turn);
-                grid[i][j].win(turn);
+            if(grid[i][j].class == null){
+                arr_new[ind_new] = 0;
             }
+            else if(grid[i][j].class == "dX"){
+                arr_new[ind_new] = 0.5;
+            }
+            else if(grid[i][j].class == "dO"){
+                arr_new[ind_new] = 1;
+            }
+            ind_new++;
         }
     }
-    if(turn == "dX") {
-        turn = "dO";
+}
+
+function NN(gridi){
+    let inputs = gridizise(gridi);
+    let outputs = brain.predict(inputs);
+    console.log(outputs);  
+}
+
+//Function, that makes essentially the same as mousePressed, but for NeuralNetwork, as it returns x,y-values as outputs
+function nnMouse(x,y){
+
+    if(turn = "dO"){
+        for(let i=0; i < cols; i++){
+            for(let j=0; j < rows; j++){
+                if(grid[i][j].locx==floor(x) && grid[i][j].locy == floor(y)){
+                    grid[i][j].press(turn);
+                    grid[i][j].win(turn);
+                }
+            }
+        }
+        /* if(turn == "dX") {
+            turn = "dO";
+        }
+        else {
+            turn = "dX";
+        } */
+        draw();
+        turn = "dX"
     }
-    else {
-        turn = "dX";
+    NN(grid);
+}
+
+function mousePressed() {
+    nnMouse(floor(mouseX/w),floor(mouseY/w));
+    if(turn == "dX"){
+        for(let i=0; i < cols; i++){
+            for(let j=0; j < rows; j++){
+                if(grid[i][j].locx==floor(mouseX/w) && grid[i][j].locy == floor(mouseY/w)){
+                    grid[i][j].press(turn);
+                    grid[i][j].win(turn);
+                }
+            }
+        }
+        /* if(turn == "dX") {
+            turn = "dO";
+        }
+        else {
+            turn = "dX";
+        } */
+        draw();
+        turn = "dO"
     }
-    draw();
 }
 
 function gameOver(turn){
@@ -63,5 +127,7 @@ function draw(){
             grid[i][j].show();
         }
     }
+    document.getElementById("turn_screen").innerHTML = turn
+    //gridizise(grid);
 }
 
--- transfer.php
+++ transfer.php
@@ -1,16 +1,19 @@
 <html>
 <body>
 
-<p>Grid size: <?php echo $_POST["grid_size"]; ?></p><br>
+Grid size: <?php echo $_POST["grid_size"]; ?><br>
 Winning line of symbols: <?php echo $_POST["winline"]; ?><br>
-
+<div id="turn_screen"></div>
 <script type="text/javascript">
 let grid_size= "<?php echo $_POST["grid_size"]?>";
 let winline = "<?php echo $_POST["winline"]?>";
 </script>
 <script src='https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.3/p5.js'></script>
+<script src='https://github.com/CodingTrain/Toy-Neural-Network-JS/blob/master/lib/nn.js'></script>
+<script src='https://github.com/CodingTrain/Toy-Neural-Network-JS/blob/master/lib/matrix.js'></script>
 <script src='sketch.js'></script>
 <script src='Tile.js'></script>
+
 
 </body>
 </html>
