da70eca712c54471bb920e6ac86c1aeba45f1233
L8317 <l8317@student.jamk.fi>
2019-02-16 19:20:04 +0200

    v 1.o ready

--- Tile.js
+++ Tile.js
@@ -18,8 +18,12 @@ Tile.prototype.show = function() {
         if(this.class == "dX"){
             rect(this.x+this.w * 0.25, this.y+this.w * 0.25, this.w * 0.5, this.w * 0.5)
         }
-        else{
+        else if(this.class == "dO"){
             ellipse(this.x+this.w * 0.5, this.y+this.w * 0.5, this.w * 0.5)
+        }
+        else {
+            fill(0);
+            rect(this.x+this.w * 0.25, this.y+this.w * 0.25, this.w * 0.2, this.w * 0.2)
         }
     } 
 }
@@ -34,19 +38,46 @@ Tile.prototype.press = function(turn) {
 
 Tile.prototype.win = function(turn){
     let k = 0;
-    for(let i=0; i < winline; i++){
+    for(let i=1; i < winline; i++){
         if(this.locx+i < grid_size && grid[this.locx+i][this.locy].class == turn && grid[this.locx+i][this.locy].class != null){
             k++;
-            console.log("What");
-            console.log(this.locx+i)
         }
-        if(this.locx-i > 0 && grid[this.locx-i][this.locy].class == turn && grid[this.locx-i][this.locy].class != null){
-            console.log("nosense");
+        if(this.locx-i >= 0 && grid[this.locx-i][this.locy].class == turn && grid[this.locx-i][this.locy].class != null){
             k++;
         }
-        console.log(k);
         if(k == winline){
             gameOver(turn);
         }
+        else { k = 0;}
+        if(this.locy < grid_size && grid[this.locx][this.locy+i].class == turn && grid[this.locx][this.locy+i].class != null){
+            k++;
+        }
+        if(this.locy-i >= 0 && grid[this.locx][this.locy-i].class == turn && grid[this.locx-i][this.locy].class != null){
+            k++;
+        }
+        if(k == winline){
+            gameOver(turn);
+        }
+        else { k = 0;}
+        if(this.locx+i < grid_size && this.locy+i < grid_size && grid[this.locx+i][this.locy+i].class == turn && grid[this.locx+i][this.locy+i].class != null){
+            k++;
+        }
+        if(this.locy-i >= 0 && this.locx-i >= 0 &&grid[this.locx-i][this.locy-i].class == turn && grid[this.locx-i][this.locy-i].class != null){
+            k++;
+        }
+        if(k == winline){
+            gameOver(turn);
+        }
+        else { k = 0;}
+        if(this.locy + i < grid_size && this.locx-i >= 0 && grid[this.locx-i][this.locy+i].class == turn && grid[this.locx-i][this.locy+i].class != null){
+            k++;
+        }
+        if(this.locy-i >= 0 && this.locx+i < grid_size && grid[this.locx+i][this.locy-i].class == turn && grid[this.locx+i][this.locy-i].class != null){
+            k++;
+        }
+        if(k == winline){
+            gameOver(turn);
+        }
+        else { k = 0;}
     }
 }
