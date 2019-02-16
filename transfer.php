b7c80692cc64e7d0f72a699098655af2061956b1
L8317 <l8317@student.jamk.fi>
2019-02-16 18:13:15 +0200

    uh

--- transfer.php
+++ transfer.php
@@ -2,11 +2,11 @@
 <body>
 
 <p>Grid size: <?php echo $_POST["grid_size"]; ?></p><br>
-Winning line of symbols: <?php echo $_POST["winline"]; ?>
+Winning line of symbols: <?php echo $_POST["winline"]; ?><br>
 
 <script type="text/javascript">
-let grid_size= "<?php $_POST["grid_size"]?>";
-let winline = "<?php $_POST["winline"]?>";
+let grid_size= "<?php echo $_POST["grid_size"]?>";
+let winline = "<?php echo $_POST["winline"]?>";
 </script>
 <script src='https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.3/p5.js'></script>
 <script src='sketch.js'></script>
